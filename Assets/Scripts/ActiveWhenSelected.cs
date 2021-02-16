537c1567ee67d2fbd2842540ac3337e4c35043cd
Felipe Henrique <solelipecosta1@gmail.com>
2021-02-16 14:57:59 -0400

    chore: subindo mudanças

--- Assets/Scripts/ActiveWhenSelected.cs
+++ Assets/Scripts/ActiveWhenSelected.cs
@@ -2,26 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ActiveWhenSelected : MonoBehaviour
 {
     private EventSystem _event;
     public GameObject imagem;
+    public GameObject texto;
+    private int selectedSize = 120;
+    private int unselectedSize = 70;
+    public float valueToAddPosition = 20;
+    private float Y;
+    private float X;
 
     void Start()
     {
         _event = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+        x = transform.postion.x;
+        y = transform.postion.y;
     }
 
     void Update()
     {
         if(_event.currentSelectedGameObject == gameObject){
-            imagem.SetActive(true);
+            ChangeImageState(true);
+            ChangeFontSize(selectedSize);
         }else{
             if(imagem.activeSelf)
             {
-                imagem.SetActive(false);
+                ChangeImageState(false);
+                ChangeFontSize(unselectedSize);
+                ChangeTextPosition(false);
             }
         }
+    }
+
+    void ChangeImageState(bool bol){
+        imagem.SetActive(bol);
+    }
+
+    void ChangeFontSize(int value)
+    {
+        texto.GetComponent<Text>().fontSize = value;
+    }
+
+    void ChangeTextPosition(bool bol)
+    {
+        if(bol)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(x, y+20);
+        }else{
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(x, y);
+        }
+
     }
 }
